6980e4d44eec33d7bacce81729cf0de733420964
Danni Popova <danni@macbook.local>
2023-07-27 22:57:15 +0100

    feat: db schema

---
+++ src/pages/routines/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+
+const Routines: NextPage = () => {
+    return (
+        <div>
+            <h1>Routines</h1>
+        </div>
+    )
+}
+
+export default Routines;
---
+++ src/pages/tasks/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+
+const Tasks: NextPage = () => {
+    return (
+        <div>
+            <h1>Tasks</h1>
+        </div>
+    )
+}
+
+export default Tasks;
